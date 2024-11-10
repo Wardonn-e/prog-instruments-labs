e890ea993752d04be240a6871e9fb814c7e1a07d
Wardonn-e <wardonne@yandex.ru>
2024-11-11 02:26:02 +0400

    Refactor line breaks and spaces

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,8 +1,9 @@
 import sys
 
+import random
 import pygame
 import colour
-import random
+
 
 sizeOfWindow = [2000, 1500]
 pygame.init()
@@ -35,62 +36,75 @@ endFlagStartingTime = False
 class ManagementGame:
     @staticmethod
     def init():
-        global playAgainR
-        global quitR
-        global player
-        player = Rect(100, 100, 1000, 1300, "white", False, False, False, False, 8, 0, False, False)
+        global playAgainR, quitR, player
+        player = Rect(
+            100, 100, 1000, 1300, "white", False, False, False,
+            False, 8, 0, False, False
+        )
         for num in range(countOfEnemy + 1):
-            Rect
-        playAgainR = Rect(300, 200, 1450, 1000, "white", False, False, False, False, 0, 5, False, False)
-        quitR = Rect(300, 200, 250, 1000, "white", False, False, False, False, 0, 5, False, False)
+            Rect()
+        playAgainR = Rect(
+            300, 200, 1450, 1000, "white", False, False, False,
+            False, 0, 5, False, False
+        )
+        quitR = Rect(
+            300, 200, 250, 1000, "white", False, False, False,
+            False, 0, 5, False, False
+        )
 
     @staticmethod
     def gameOver():
-        global gameOver
+        global gameOver, running, player
         gameOver = True
-        pygame.display.update()
-        global running
         running = False
-        ManagementGame.claerEnemy()
+        pygame.display.update()
+        ManagementGame.clearEnemy()
         print('hi')
-        global player
 
     @staticmethod
     def showTitle(score):
         global wave
         textScore = pygame.font.Font(None, 80)
-        win.blit(textScore.render(f"Score: {score}", False, (255, 255, 255)), (0, 0))
+        win.blit(
+            textScore.render(f"Score: {score}", False, (255, 255, 255)),
+            (0, 0)
+        )
         textScore = pygame.font.Font(None, 60)
-        win.blit(textScore.render(f"wave: {wave}", False, (255, 0, 255)), (0, 60))
+        win.blit(
+            textScore.render(f"wave: {wave}", False, (255, 0, 255)),
+            (0, 60)
+        )
         if gameOver:
             ManagementGame.GameOverTitle()
 
     @staticmethod
     def GameOverTitle():
-        global playAgainR
-        global quitR
-        global player
-        global running
-        global generateEnemy
-        global gameOver
-        global wave
-        global lastWaveScore
-        global score
-        global defaultStepWave
-        global defaultStepWave
-        global stepWave
+        global playAgainR, quitR, player, running, generateEnemy
+        global gameOver, wave, lastWaveScore, score, stepWave
+
         text = pygame.font.Font(None, 250)
         text2 = pygame.font.Font(None, 100)
-        win.blit(text.render(f"GAME OVER", False, (255, 0, 0)), (460, 650))
-        win.blit(text2.render(f"final score: {score}", False, (255, 255, 255)), (720, 850))
-        global playAgainR
-        global quitR
+        win.blit(
+            text.render("GAME OVER", False, (255, 0, 0)),
+            (460, 650)
+        )
+        win.blit(
+            text2.render(f"final score: {score}", False, (255, 255, 255)),
+            (720, 850)
+        )
+
         playAgainR.show()
         quitR.show()
-        if (playAgainR.x < player.x < playAgainR.x + playAgainR.w) or (
-                player.x < playAgainR.x < player.x + player.w) or (player.x == playAgainR.x):
-            if (playAgainR.y < player.y < playAgainR.y + playAgainR.h) or (
-                    player.y < playAgainR.y < player.y + player.h):
+
+        if (
+            (playAgainR.x < player.x < playAgainR.x + playAgainR.w) or
+            (player.x < playAgainR.x < player.x + player.w) or
+            (player.x == playAgainR.x)
+        ):
+            if (
+                (playAgainR.y < player.y < playAgainR.y + playAgainR.h) or
+                (player.y < playAgainR.y < player.y + player.h)
+            ):
                 running = True
                 generateEnemy = True
                 gameOver = False
@@ -99,41 +113,48 @@ class ManagementGame:
                 score = 0
                 wave = 1
 
-        if (quitR.x < player.x < quitR.x + quitR.w) or (player.x < quitR.x < player.x + player.w) or (
-                player.x == quitR.x):
-            if (quitR.y < player.y < quitR.y + quitR.h) or (player.y < quitR.y < player.y + player.h):
+        if (
+            (quitR.x < player.x < quitR.x + quitR.w) or
+            (player.x < quitR.x < player.x + player.w) or
+            (player.x == quitR.x)
+        ):
+            if (
+                (quitR.y < player.y < quitR.y + quitR.h) or
+                (player.y < quitR.y < player.y + player.h)
+            ):
                 pygame.quit()
                 sys.exit()
+
         playAgainT = pygame.font.Font(None, 100)
         quitT = pygame.font.Font(None, 100)
-        win.blit(playAgainT.render(f"again", False, (255, 255, 255)), (1510, 1060))
-        win.blit(quitT.render(f"quit", False, (255, 255, 255)), (330, 1060))
+        win.blit(
+            playAgainT.render("again", False, (255, 255, 255)),
+            (1510, 1060)
+        )
+        win.blit(
+            quitT.render("quit", False, (255, 255, 255)),
+            (330, 1060)
+        )
 
     @staticmethod
-    def claerEnemy():
+    def clearEnemy():
         enemiesList.clear()
         player.x = 940
         player.y = 1100
 
     @staticmethod
     def manageWave():
-        global lastWaveScore
-        global stepWave
-        global generateEnemy
-        global wave
-        global showTitleWave
-        global startingTime
-        global running
-        global endFlagStartingTime
-        global countOfEnemy
-        if ((score - lastWaveScore) - stepWave) > 0:
+        global lastWaveScore, stepWave, generateEnemy, wave
+        global startingTime, running, endFlagStartingTime, countOfEnemy
+
+        if (score - lastWaveScore - stepWave) > 0:
             lastWaveScore = score
             wave += 1
             countOfEnemy += 2
             stepWave += countOfEnemy * 2
             startingTime = True
             running = False
-            ManagementGame.claerEnemy()
+            ManagementGame.clearEnemy()
         ManagementGame.checkTime(1000)
         if endFlagStartingTime and not gameOver:
             generateEnemy = True
@@ -142,13 +163,14 @@ class ManagementGame:
 
         if startingTime:
             textWave = pygame.font.Font(None, 80)
-            win.blit(textWave.render(f"Wave: {wave}", False, (255, 255, 255)), (450, 650))
+            win.blit(
+                textWave.render(f"Wave: {wave}", False, (255, 255, 255)),
+                (450, 650)
+            )
 
     @staticmethod
     def checkTime(count):
-        global iForWave
-        global startingTime
-        global endFlagStartingTime
+        global iForWave, startingTime, endFlagStartingTime
         iForWave += 1
         if iForWave - count == 0:
             iForWave = 0
@@ -161,7 +183,7 @@ class ManagementGame:
     def intoWhile():
         ManagementGame.showTitle(score)
         ManagementGame.manageWave()
-        keys = pygame.key.get_pressed()  # checking pressed keys
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             player.moveY(False)
         if keys[pygame.K_DOWN]:
@@ -199,10 +221,14 @@ class ManagementGame:
 
 class Color:
     colorRGB = None
-    nameOfSomeColor = ['DarkGreen', 'Green', 'DarkCyan', 'DeepSkyBlue', 'DarkTurquoise', 'MediumSpringGreen', 'Lime',
-                       'SpringGreen', 'Cyan', 'Aqua', 'MidnightBlue', 'DodgerBlue', 'LightSeaGreen', 'ForestGreen',
-                       'SeaGreen', 'DarkSlateGray', 'DarkSlateGrey', 'LimeGreen', 'MediumSeaGreen', 'Turquoise',
-                       'RoyalBlue', 'SteelBlue', 'DarkSlateBlue', 'MediumTurquoise']
+    nameOfSomeColor = [
+        'DarkGreen', 'Green', 'DarkCyan', 'DeepSkyBlue', 'DarkTurquoise',
+        'MediumSpringGreen', 'Lime', 'SpringGreen', 'Cyan', 'Aqua',
+        'MidnightBlue', 'DodgerBlue', 'LightSeaGreen', 'ForestGreen',
+        'SeaGreen', 'DarkSlateGray', 'DarkSlateGrey', 'LimeGreen',
+        'MediumSeaGreen', 'Turquoise', 'RoyalBlue', 'SteelBlue',
+        'DarkSlateBlue', 'MediumTurquoise'
+    ]
 
     def __init__(self, color="white"):
         self.getRGB(color)
@@ -235,9 +261,11 @@ class Rect:
     randSpead = None
     randBorder = None
 
-    def __init__(self, w=0, h=0, x=0, y=0, color="white", randColor=True, randSize=True, randPositionX=True,
-                 randSpead=True, spead=5,
-                 border=5, append=True, randBorder=True):
+    def __init__(
+            self, w=0, h=0, x=0, y=0, color="white",
+            randColor=True, randSize=True, randPositionX=True,
+            randSpead=True, spead=5, border=5, append=True, randBorder=True
+    ):
         self.randColor = randColor
         self.x = x
         self.y = y
@@ -257,14 +285,9 @@ class Rect:
             enemiesList.append(self)
 
     def moveY(self, direction=True):
-        if direction:
-            self.y += self.spead
-            self.show()
-            self.destroyer()
-        else:
-            self.y -= self.spead
-            self.show()
-            self.destroyer()
+        self.y += self.spead if direction else -self.spead
+        self.show()
+        self.destroyer()
 
     def random(self):
         if self.randColor:
@@ -279,23 +302,17 @@ class Rect:
         if self.randSpead:
             self.spead = random.randint(1, 10)
         if self.randBorder:
-            if random.randint(0, 1):
-                self.border = random.randint(4, 15)
-            else:
-                self.border = 0
+            self.border = random.randint(4, 15) if random.randint(0, 1) else 0
 
     def moveX(self, direction=True):
-        if direction:
-            self.x += self.spead
-            self.show()
-            self.destroyer()
-        else:
-            self.x -= self.spead
-            self.show()
-            self.destroyer()
+        self.x += self.spead if direction else -self.spead
+        self.show()
+        self.destroyer()
 
     def show(self):
-        pygame.draw.rect(win, self.color, ((self.x, self.y), (self.w, self.h)), self.border)
+        pygame.draw.rect(
+            win, self.color, ((self.x, self.y), (self.w, self.h)), self.border
+        )
 
     def destroyer(self):
         if self.y > (sizeOfWindow[1] + 200):
@@ -304,8 +321,15 @@ class Rect:
             score += 1
 
     def check(self):
-        if (self.x < player.x < self.x + self.w) or (player.x < self.x < player.x + player.w) or (player.x == self.x):
-            if (self.y < player.y < self.y + self.h) or (player.y < self.y < player.y + player.h):
+        if (
+            (self.x < player.x < self.x + self.w) or
+            (player.x < self.x < player.x + player.w) or
+            (player.x == self.x)
+        ):
+            if (
+                (self.y < player.y < self.y + self.h) or
+                (player.y < self.y < player.y + player.h)
+            ):
                 ManagementGame.gameOver()
 
 
@@ -317,4 +341,4 @@ while True:
     for item in events:
         if item.type == pygame.QUIT:
             pygame.quit()
-            pygame.sys.exit()
+            sys.exit()
